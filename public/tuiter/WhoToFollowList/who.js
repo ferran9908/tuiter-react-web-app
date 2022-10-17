40a4ffbb7ca15a104f9279f05e1faa1647d25a79
Ferran <ferran.9908@gmail.com>
2022-10-16 20:24:44 -0700

    Implement Who To Follow Column

--- public/tuiter/WhoToFollowList/who.js
+++ public/tuiter/WhoToFollowList/who.js
@@ -1,15 +1,32 @@
 export default [
-  { avatarIcon: "java.png", userName: "Java", handle: "Java" },
   {
-    avatarIcon: "relativity.jpeg",
+    avatarIcon:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1024px-Java_programming_language_logo.svg.png",
+    userName: "Java",
+    handle: "Java",
+  },
+  {
+    avatarIcon:
+      "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/j3dbh6kok0dket3uz2vy",
     userName: "Relativity Space",
     handle: "relativityspace",
   },
   {
-    avatarIcon: "virgin.png",
+    avatarIcon:
+      "https://play-lh.googleusercontent.com/mt0xEz5iyNO0HYXjX7aXSVcS1r0lGl-FQN2ipMEDyuRJVbHM7zx98kN8tXj-wykP4-E",
     userName: "Virgin Galactic",
     handle: "virgingalactic",
   },
-  { avatarIcon: "nasa.png", userName: "NASA", handle: "NASA" },
-  { avatarIcon: "tesla.png", userName: "Tesla", handle: "Tesla" },
+  {
+    avatarIcon:
+      "https://i.pinimg.com/originals/dd/f5/0e/ddf50e1451baf41d1aa01947fe9452bd.jpg",
+    userName: "NASA",
+    handle: "NASA",
+  },
+  {
+    avatarIcon:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Tesla_T_symbol.svg/800px-Tesla_T_symbol.svg.png",
+    userName: "Tesla",
+    handle: "Tesla",
+  },
 ];
--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,4 +1,5 @@
 import NavigationSidebar from "../NavigationSideBar/index.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
 
 function exploreComponent() {
   $("#wd-explore").append(`
@@ -11,8 +12,9 @@ function exploreComponent() {
         <div class="col-10 col-lg-7 col-xl-6 bg-primary">
          <h3>ExploreComponent</h3>
         </div>
-        <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 bg-danger">
-         <h3>WhoToFollowList </h3>
+        <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+         <!-- <h3>WhoToFollowList </h3> -->
+         ${WhoToFollowList()}
         </div>
        </div>
 
