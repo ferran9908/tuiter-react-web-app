5f9c48958c25a2e2ec1f2c2e3babb25a03fc0099
Ferran <ferran.9908@gmail.com>
2022-10-23 20:04:59 -0700

    Add Redirects

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,10 +1,12 @@
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
 
 function Tuiter() {
   return (
     <div>
       <Nav />
       <h1>Tuiter</h1>
+      <NavigationSidebar active='home' />
     </div>
   );
 }
