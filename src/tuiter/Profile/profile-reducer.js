5d6b344fe77c3d14bbde3f951c25cc18f06fb3d6
Ferran <ferran.9908@gmail.com>
2022-11-06 20:23:16 -0800

    Change Banner

--- src/tuiter/Profile/profile-reducer.js
+++ src/tuiter/Profile/profile-reducer.js
@@ -6,8 +6,7 @@ const currentUser = {
   handle: "@jannunzi",
   profilePicture:
     "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg",
-  bannerPicture:
-    "https://opengraph.githubassets.com/1d51d76c0a42a99627df3cf99b44b1a1f6001f34eb915b38d4c23df36d1a9e13/vmchale/polyglot",
+  bannerPicture: "https://www.cs.cmu.edu/~aldrich/courses/17-396/protobg.jpg",
   bio: "Faculty, Software Engineer, AI, Space, and renewable enthusiast. Retuits and likes are not endorsements.",
   website: "youtube.com/webdevtv",
   location: "Boston, MA",
